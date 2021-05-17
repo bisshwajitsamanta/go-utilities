b1ba95801d2dd3c4dc9b4d620b7a62deb9a14e99
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2021-05-17 19:19:59 +0530

    Create Buckets in S3 using Golang

--- lambda/s3Operations/main.go
+++ lambda/s3Operations/main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"os"
 )
 
 var s3session *s3.S3
 
+const (
+	BUCKET_NAME = "bishwajitsamanta9861"
+	REGION      = "us-east-1"
+)
+
 func init() {
 	s3session = s3.New(session.Must(session.NewSession(&aws.Config{
-		Region: aws.String("us-east-1"),
+		Region: aws.String(REGION),
 	})))
 }
 
@@ -24,6 +31,29 @@ func ListBuckets() (resp *s3.ListBucketsOutput) {
 	return resp
 }
 
+func CreateBucket() (resp *s3.CreateBucketOutput) {
+
+	resp, err := s3session.CreateBucket(&s3.CreateBucketInput{
+		Bucket: aws.String(BUCKET_NAME),
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case s3.ErrCodeBucketAlreadyExists:
+				fmt.Println("Bucket already Exists")
+				os.Exit(1)
+			case s3.ErrCodeBucketAlreadyOwnedByYou:
+				fmt.Println("You already owned this Bucket!")
+			default:
+				panic(err)
+			}
+		}
+	}
+	return resp
+}
+
 func main() {
+	fmt.Println(ListBuckets())
+	fmt.Println(CreateBucket())
 	fmt.Println(ListBuckets())
 }
